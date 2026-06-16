e23a3055bec61c16330272c12341ddf836b49a9a
Suyog Mahajan <mahajansuyog9322@gmail.com>
2026-06-17 00:20:56 +0530

    Defined DLL Statements for Bronze Layer and schema creation

---
+++ scripts/bronze/DLL.sql
@@ -0,0 +1,59 @@
+CREATE database if not exists SDW_BRONZE;
+CREATE database if not exists SDW_SILVER;
+CREATE database if not exists SDW_GOLD;
+
+CREATE TABLE IF NOT EXISTS SDW_BRONZE.CRM_CUST_INFO 
+(
+cst_id INT,
+cst_key VARCHAR(50),
+cst_firstname VARCHAR(50),
+cst_lastname VARCHAR(50),
+cst_marital_status VARCHAR(50),
+cst_gndr VARCHAR(50),
+cst_create_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS SDW_BRONZE.CRM_PRD_INFO
+(
+prd_id INT,
+prd_key VARCHAR(50),
+prd_nm VARCHAR(50),
+prd_cost INT,
+prd_line VARCHAR(50),
+prd_start_dt DATE,
+prd_end_dt DATE
+);
+
+CREATE TABLE IF NOT EXISTS SDW_BRONZE.CRM_SALES_DETAILS
+(
+sls_ord_num VARCHAR(50),
+sls_prd_key VARCHAR(50),
+sls_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+
+CREATE TABLE IF NOT EXISTS SDW_BRONZE.ERP_CUST_AZ12
+(
+CID VARCHAR(50),
+BDATE DATE,
+GEN VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS SDW_BRONZE.ERP_LOC_A101
+(
+CID VARCHAR(50),
+CNTRY VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS SDW_BRONZE.ERP_PXCAT_G1V2
+(
+ID VARCHAR(50),
+CAT VARCHAR(50),
+SUBCAT VARCHAR(50),
+MAINTENANCE VARCHAR(50)
+);
